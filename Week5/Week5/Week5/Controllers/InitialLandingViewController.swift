a100e1a313949b0ed852e303aaa87fedfc8537bb
charlesbotello <charlesabotello@gmail.com>
2021-04-26 23:02:36 -0500

    Add image to initial page

--- Week5/Week5/Week5/Controllers/InitialLandingViewController.swift
+++ Week5/Week5/Week5/Controllers/InitialLandingViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class InitialLandingViewController: UIViewController {
 
+    var personOne = Person(name: "John Wick", details: "Part time Husband, Part time assassin with anger issues", imageName: "John-Wick")
+    var personTwo = Person(name: "Michael Corleone", details: "Misunderstood bussiness man,has an idiot for a brother", imageName: "Michael-Corleone")
+    
+    @IBOutlet weak var personImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        loadImage(path: personOne.imageName, imageView: personImageView)
     }
 
 
+    func loadImage(path: String, imageView: UIImageView!){
+        let imageName = path
+        let image = UIImage(named: imageName)
+        imageView.image = image
+    }
 }
 
