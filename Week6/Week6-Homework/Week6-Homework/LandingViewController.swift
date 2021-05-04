df6fe36a8e3fe0f02ebf6d83f809c369f7396923
charlesbotello <charlesabotello@gmail.com>
2021-05-04 18:07:29 -0500

    Initial storyboard with constraints

---
+++ Week6/Week6-Homework/Week6-Homework/LandingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week6-Homework
+//
+//  Created by Charlie Botello on 5/4/21.
+//
+
+import UIKit
+
+class LandingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
